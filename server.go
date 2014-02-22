ae5a0cdf97b1db6172d12012f42820d4b4953210
Collin Glass <collinglass@gmx.com>
2014-02-22 11:02:05 -0500

    Basic CSV reader

--- server.go
+++ server.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
+type hourlyVisit struct {
+	Hour           int `field: Hour`
+	UniqueVisitors int
+}
+
+func readCSV() {
+	csvFile, err := os.Open("data/traffic.csv")
+	defer csvFile.Close()
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(csvFile)
+	for {
+		fields, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		fmt.Print(fields)
+	}
+}
+
 func main() {
+	readCSV()
+
 	log.Println("Starting Server")
 	http.Handle("/", http.FileServer(http.Dir("./app/")))
 
